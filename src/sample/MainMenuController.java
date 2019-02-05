8a09a9da6f8080e149034660bd7f6da70095a52f
Unknown <rwu17@student.aau.dk>
2019-02-05 21:37:25 +0100

    Blend effect

--- src/sample/MainMenuController.java
+++ src/sample/MainMenuController.java
@@ -1,8 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+//import javax.swing.text.html.ImageView;
+//import java.awt.*;
+import java.awt.color.*;
+
+import javafx.scene.control.Button;
+import javafx.scene.effect.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.effect.Blend;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
 public class MainMenuController {
+    @FXML ImageView recipeNew;
+    @FXML VBox recipeCreation;
+    @FXML Button toMainMenu;
 
     public void recipeCreateMenu(){
+        recipeCreation.setVisible(true);
         System.out.println("create a new recipe");
     }
 
@@ -11,7 +28,24 @@ public class MainMenuController {
     }
 
     public void blendImage(){
+        Blend blend = new Blend();
+        blend.setMode(BlendMode.COLOR_BURN);
+        ColorInput blendColorInput = new ColorInput();
+        blendColorInput.setPaint(Color.STEELBLUE);
+        blendColorInput.setX(0);
+        blendColorInput.setY(0);
+        blendColorInput.setWidth(95);
+        blendColorInput.setHeight(110);
+        blend.setTopInput(blendColorInput);
+        recipeNew.setEffect(blend);
+    }
 
+    public void removeEffect(){
+        recipeNew.setEffect(null);
+    }
+
+    public void backToMain(){
+        recipeCreation.setVisible(false);
     }
 
 }
